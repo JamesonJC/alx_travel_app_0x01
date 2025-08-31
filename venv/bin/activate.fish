add5e5991b08a6c25976168a4985da433dc77b40
JamesonJC <41649261+JamesonJC@users.noreply.github.com>
2025-08-31 20:03:36 +0000

    Milestone 4: Payment Integration with Chapa API

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/alx_travel_app/venv"
+set -gx VIRTUAL_ENV "/workspaces/alx_travel_app_0x02/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
